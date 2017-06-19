f2de8a5afec499e72c1bdca18cac6120a7ecbde6
Katie Tanner <katiejen81@gmail.com>
2017-06-18 21:19:56 -0500

    Added License information

--- Logbook_Harvest.py
+++ Logbook_Harvest.py
@@ -4,7 +4,20 @@
 Google Logbook Harvester
 Created on Wed May 31 21:41:35 2017
 Written in Python2
-@author: katie
+Copyright (C) 2017  Kathryn Tanner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 #Import our packages
