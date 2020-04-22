af126bfeb3e62f51e1d119d8232988916f0a3685
Mike Tanner <mtanner17@yaho.com>
2020-04-21 21:12:04 -0500

    Added the path variable update to make sure that it is sourcing the right binaries

--- Logbook.sh
+++ Logbook.sh
@@ -1,15 +1,14 @@
+#!/usr/bin/env bash
+
 # @Author: katie
 # @Date:   2018-04-15T16:57:13-05:00
 # @Last modified by:   katie
 # @Last modified time: 2018-04-15T17:06:29-05:00
 
-
-
-#!/bin/bash
-
+export PATH="/home/mike/miniconda3/bin:$PATH"
 cd '/home/mike/apps/Logbook'
 
-conda activate logbook
+source activate logbook
 
 python Logbook_Harvest.py
 python Logbook_Print.py
